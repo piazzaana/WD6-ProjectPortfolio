2ec6c38fc276ce5dda5294ae076240c649f81b61
Ana Piazza <ana.piazza@yahoo.com>
2018-09-05 19:00:24 -0400

    Displayed info from the database to the screen

--- Solo Work/Archive_f3a2e113-d4da-42ce-ab58-b2c23fe8aaf3/shopportunity/routes/index.js
+++ Solo Work/Archive_f3a2e113-d4da-42ce-ab58-b2c23fe8aaf3/shopportunity/routes/index.js
@@ -4,14 +4,14 @@ const Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Product.find(function (err, docs) {
-      let productChunks =[];
-      let chunkSize = 3;
-      for(let i = 0; i < docs.length; i += chunkSize){
-        productChunks.push(docs.slice(i, i + chunkSize));
-      }
-      res.render('index', { title: 'Shopportunity', products: productChunks });
-  });
+    Product.find(function (err, docs) {
+        let productChunks = [];
+        let chunkSize = 3;
+        for(let i = 0; i < docs.length; i += chunkSize){
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('index', { title: 'Shopportunity', products: productChunks });
+    });
 });
 
 module.exports = router;
