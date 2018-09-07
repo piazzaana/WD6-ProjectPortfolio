e2ac0589bf82d8d45e0adaff44b4e6b881a89b21
Ana Piazza <ana.piazza@yahoo.com>
2018-09-07 16:43:40 -0400

    added csrf protection, it works

--- Solo Work/Archive_f3a2e113-d4da-42ce-ab58-b2c23fe8aaf3/shopportunity/app.js
+++ Solo Work/Archive_f3a2e113-d4da-42ce-ab58-b2c23fe8aaf3/shopportunity/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret:"mysecret", resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
