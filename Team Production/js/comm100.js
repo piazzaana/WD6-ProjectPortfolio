787e2ab590291477001e04ba7344ba48be5a7c54
angdionne <30736485+angdionne@users.noreply.github.com>
2018-09-14 02:57:36 +0600

    Update code to Splash page

--- Team Production/js/comm100.js
+++ Team Production/js/comm100.js
@@ -1,31 +1,31 @@
 var AllenSports = AllenSports || {};
 
-AllenSports.Comm100 = (function () {
-    var comm100Triggers = document.querySelectorAll('.header-contact__item--chat, .home__help-box--chat');
-    var url = 'https://chatserver.comm100.com/chatwindow.aspx?planId=535&siteId=65128&pageUrl=http%3A%2F%2Fwww.allensportswear.com%2F&newurl=1&r=5';
-    var windowName = 'Chat';
-    var codePlan = 535;
+// AllenSports.Comm100 = (function () {
+//     var comm100Triggers = document.querySelectorAll('.header-contact__item--chat, .home__help-box--chat');
+//     var url = 'https://chatserver.comm100.com/chatwindow.aspx?planId=535&siteId=65128&pageUrl=http%3A%2F%2Fwww.allensportswear.com%2F&newurl=1&r=5';
+//     var windowName = 'Chat';
+//     var codePlan = 535;
 
-    var init = function () {
-        for (var i = 0; i < comm100Triggers.length; i++) {
-            comm100Triggers[i].addEventListener('click', function (e) {
-                e.preventDefault();
-                Comm100API.open_chat_window(null, codePlan);
-            });
-        }
+//     var init = function () {
+//         for (var i = 0; i < comm100Triggers.length; i++) {
+//             comm100Triggers[i].addEventListener('click', function (e) {
+//                 e.preventDefault();
+//                 Comm100API.open_chat_window(null, codePlan);
+//             });
+//         }
 
-        document.getElementById('comm100-button-535').style.display = 'none';
-        let comm100Button = document.getElementById('comm100-float-button-2');
-        if (comm100Button) {
-          comm100Button.style.display = 'none';
-        }
-    };
+//         document.getElementById('comm100-button-535').style.display = 'none';
+//         let comm100Button = document.getElementById('comm100-float-button-2');
+//         if (comm100Button) {
+//           comm100Button.style.display = 'none';
+//         }
+//     };
 
-    return {
-        init: init,
-        codePlan: codePlan
-    };
-})();
+//     return {
+//         init: init,
+//         codePlan: codePlan
+//     };
+// })();
 
 var Comm100API = Comm100API || {};
 
