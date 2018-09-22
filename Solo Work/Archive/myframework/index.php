d22900955d42c19614d6f9af4df23c6be58897fb
Ana Piazza <piazzaana@fullsail.edu>
2018-09-21 20:05:10 -0400

    cleaned the repo from all of the other stuff

--- Solo Work/Archive/myframework/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-//app start
-include 'bin/config.php';
--- Solo Work/Archive/myframework/views/welcome.php
+++
@@ -1,9 +0,0 @@
-<h1>STUDENTS GRADES REPORT (Teacher's App)</h1>
-
-<h3 style="color: green">Input your student's name & final grade percentage (%)</h3>
-
-<form method="post" action="/welcome/inputRcv">
-    <input type="text" id="student" name="student" placeholder="Name">
-    <input type="text" id="grade" name="grade" placeholder="grade">
-    <button type="submit">Submit Query</button>
-</form>
